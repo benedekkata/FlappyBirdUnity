80c495dcab7c848693af6aa2e4507a4e40a3b58b
Benedek Katalin <benedek.kata99@gmail.com>
2024-01-13 18:16:27 +0000

    High Score implementation

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -8,14 +8,26 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public Text highScoreText;
     public GameObject gameOverScreen;
     public PipeSpawnScript pipeSpawnScript;
+
+    private void Start()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
 
     [ContextMenu("Increase Score")]
     public void addScore (int scoreToAdd) 
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+        if (playerScore > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", playerScore);
+            highScoreText.text = playerScore.ToString();
+        }
+        
     }
 
     public void RestartGame()
